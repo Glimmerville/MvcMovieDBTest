bcc456a63a9fec0310e9326182d256acb56016eb
Your Name <you@email.com>
2016-05-07 20:46:54 -0400

    initial commit

---
+++ MyMvcMovie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMvcMovie.Startup))]
+namespace MyMvcMovie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
